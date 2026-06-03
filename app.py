100923a895e8c772d0e50c13c5ddcdade6f92a57
Anniina <anniina.leikas@outlook.com>
2026-06-03 18:38:50 +0300

    Updated code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Podman container"
+    return "Hello from Podman container!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
